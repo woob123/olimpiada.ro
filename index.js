dc0d0a24ba8f646cfd0eab27bae0b13f51611112
den <142715769+woob123@users.noreply.github.com>
2024-06-06 20:26:31 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 100.00,
+    minWidth: 100.00,
+    scale: 3.00,
+    scaleMobile: 1.00,
+    color: 0x1f2335,
+    shininess: 3.00,
+    waveHeight: 15.00,
+    waveSpeed: 0.3,
+    zoom: 1.00,
+    backgroundColor: 0x1f2335
+});
+
